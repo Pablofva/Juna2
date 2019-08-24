ba34dc77d944c94fa322d2f23dfb937f766c7686
AlexSalazar2019 <ale77_salazar@hotmail.com.ar>
2019-08-24 16:51:04 -0300

    codigo css y plantilla mas legibles

--- resources/views/modulos/head.blade.php
+++ resources/views/modulos/head.blade.php
@@ -1,26 +1,30 @@
 <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 
-        <title>JUNA</title>
+<title>JUNA</title>
 
-        <!-- Fuentes -->
-        <link href="{{asset('https://fonts.googleapis.com/css?family=Acme|Nunito|Fjalla+One|Oswald|Russo+One&display=swap')}}" rel="stylesheet">
+<!-- Fuentes -->
+<link href="{{asset('https://fonts.googleapis.com/css?family=Acme|Nunito|Fjalla+One|Oswald|Russo+One&display=swap')}}" rel="stylesheet">
 
-        <!-- Bootstrap -->
-        
-        <link rel="stylesheet" href="{{asset('https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css')}}"  >
-        <!-- <link rel="stylesheet" href="{{asset('css/app.css')}}"> -->
-        <!-- Estilos -->
-        <link rel="stylesheet" href="{{asset('css/style.css')}}">
+<!-- librerias de Bootstrap -->
+<link rel="stylesheet" href="{{asset('css/librerias/bootstrap.min.css')}}"  >
 
-        <!-- Iconos -->
-        <script src="{{asset('js/all.js')}}"></script>
-        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        
-        <!-- Favicon -->
-        <link rel="icon" href="{{asset('favicon.ico')}}">
+<!-- Estilos generales -->
+<link rel="stylesheet" href="{{asset('css/style.css')}}">
 
-        <!-- Bootstrap DATATABLES -->
-        <link rel="stylesheet" href="{{asset('https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css')}}">
+<!-- Estilos del pie de pagina -->
+<link rel="stylesheet" href="{{asset('css/pie.css')}}">
+
+<!-- Estilos de la cabecera de pagina -->
+<link rel="stylesheet" href="{{asset('css/cabecera.css')}}">
+
+<!-- Estilos de los botones - colores  -->
+<link rel="stylesheet" href="{{asset('css/color-botones.css')}}">
+
+<!-- Estilos para el responsive  -->
+<link rel="stylesheet" href="{{asset('css/media-queries.css')}}">
+
+<!-- Favicon -->
+<link rel="icon" href="{{asset('favicon.ico')}}">
 
 
